e0a22d26c8321b74cb95a6dae248b010a1b7c340
PhilipVigus <philipvigus@yahoo.co.uk>
2022-04-01 18:03:41 +0100

    Pass test for when age is 9

--- src/main/java/katas/lvl8/CharProblem.java
+++ src/main/java/katas/lvl8/CharProblem.java
@@ -2,7 +2,9 @@ package katas.lvl8;
 
 // https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1
 public class CharProblem {
-    public int howOld(final String ageString) {
-        return 5;
-    }
+  public int howOld(final String ageString) {
+    char ageChar = ageString.charAt(0);
+
+    return Character.getNumericValue(ageChar);
+  }
 }
--- src/test/java/katas/lvl8/CharProblemTest.java
+++ src/test/java/katas/lvl8/CharProblemTest.java
@@ -6,15 +6,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CharProblemTest {
-    CharProblem charProblem;
+  CharProblem charProblem;
 
   @BeforeEach
   void setUp() {
-      charProblem = new CharProblem();
+    charProblem = new CharProblem();
   }
 
-    @Test
+  @Test
   void returns5WhenTheAgeIs5() {
     assertEquals(5, charProblem.howOld("5 years old"));
+  }
+
+  @Test
+  void returns9WhenTheAgeIs9() {
+    assertEquals(9, charProblem.howOld("9 years old"));
   }
 }
