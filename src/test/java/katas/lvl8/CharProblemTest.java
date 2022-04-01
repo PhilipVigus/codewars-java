29515f24a8d97dd1f697a69509e2b44e197a6cbf
PhilipVigus <philipvigus@yahoo.co.uk>
2022-04-01 18:14:35 +0100

    Rename tests

--- src/test/java/katas/lvl8/CharProblemTest.java
+++ src/test/java/katas/lvl8/CharProblemTest.java
@@ -14,17 +14,17 @@ class CharProblemTest {
   }
 
   @Test
-  void returns5WhenTheAgeIs5() {
+  void returns5WhenTheAgeIs5Test() {
     assertEquals(5, charProblem.howOld("5 years old"));
   }
 
   @Test
-  void returns9WhenTheAgeIs9() {
+  void returns9WhenTheAgeIs9Test() {
     assertEquals(9, charProblem.howOld("9 years old"));
   }
 
   @Test
-  void returns9WhenTheAgeIs1() {
+  void returns9WhenTheAgeIs1Test() {
     assertEquals(1, charProblem.howOld("1 years old"));
   }
 }
