5e900eb2f29a66681a4acdf4d94cafafdbd1cf06
PhilipVigus <philipvigus@yahoo.co.uk>
2022-04-03 12:22:29 +0100

    Pass test for empty string

---
+++ src/main/java/katas/lvl7/Spacify.java
@@ -0,0 +1,7 @@
+package katas.lvl7;
+
+public class Spacify {
+  public String spacify(String str) {
+    return "";
+  }
+}
---
+++ src/test/java/katas/lvl7/SpacifyTest.java
@@ -0,0 +1,22 @@
+package katas.lvl7;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SpacifyTest {
+  Spacify spacify;
+
+  @BeforeEach
+  void setUp() {
+    spacify = new Spacify();
+  }
+
+  @Test
+  @DisplayName("It expands the empty string correctly")
+  void itExpandsTheEmptyStringCorrectly() {
+    assertEquals("", spacify.spacify(""));
+  }
+}
